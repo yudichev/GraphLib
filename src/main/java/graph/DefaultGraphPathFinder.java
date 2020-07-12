2b6b0867898217bd3889b05a0126287c1eb6cbda
CORP-HR\Valery.Yudichev <valery.yudichev@hireright.com>
2020-07-12 20:22:10 +0300

    Fix path finding

--- src/main/java/graph/DefaultGraphPathFinder.java
+++ src/main/java/graph/DefaultGraphPathFinder.java
@@ -42,7 +42,6 @@ final class DefaultGraphPathFinder implements GraphPathFinder<Edge>
 	public List<Edge> find(int from, int to)
 	{
 		Set<Integer> passedVertices = new HashSet<>();
-		passedVertices.add(from);
 		List<Edge> foundSubPath = findReversedSubPath(from, to, 1, passedVertices );
 		Collections.reverse(foundSubPath);
 		return Collections.unmodifiableList(foundSubPath);
@@ -68,7 +67,7 @@ final class DefaultGraphPathFinder implements GraphPathFinder<Edge>
 			if(intermediateEdges.isEmpty())
 				return Collections.emptyList();
 
-			passedVertices.addAll(intermediateEdges.stream().map(Edge::getTo).collect(Collectors.toSet()));
+			passedVertices.addAll(intermediateEdges.stream().map(Edge::getFrom).collect(Collectors.toSet()));
 
 			Optional<List<Edge>> reversedSubPath = edges.get(false).stream()
 					.filter(edge -> !passedVertices.contains(edge.getTo())).map(edge ->
