59103fc83f532864847b5cdbf89f969408ef2a8e
CORP-HR\Valery.Yudichev <valery.yudichev@hireright.com>
2020-07-13 17:57:18 +0300

    Correct

--- src/test/java/graph/TestGraphMultiThreaded.java
+++ src/test/java/graph/TestGraphMultiThreaded.java
@@ -47,11 +47,11 @@ public class TestGraphMultiThreaded
 			graph.addEdge(new Edge(v4, v2));
 
 			List<Edge> path = graph.getPath(v1, v4);
-			Assert.assertFalse(path.isEmpty());
+			Assert.assertFalse("Path " + v1 + "->" + v4 +" not found for graph: " + graph.toString(), path.isEmpty());
 		}
 		catch(Throwable ex)
 		{
-			Assert.fail("No exception is expected here. Thrown: " + ex.getClass());
+			Assert.fail("No exception is expected here. Thrown: " + ex.getClass() + " Message: " + ex.getMessage());
 		}
 		finally
 		{
