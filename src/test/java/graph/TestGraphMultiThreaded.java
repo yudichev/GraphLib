89ce8b0599b32081304e4da13eb723d628357e75
CORP-HR\Valery.Yudichev <valery.yudichev@hireright.com>
2020-07-13 10:28:09 +0300

    Add shutdown

--- src/test/java/graph/TestGraphMultiThreaded.java
+++ src/test/java/graph/TestGraphMultiThreaded.java
@@ -23,31 +23,37 @@ public class TestGraphMultiThreaded
 		CountDownLatch endLatch = new CountDownLatch(5);
 		ExecutorService executorService = Executors.newCachedThreadPool();
 
-		for(int i = 0; i < 5; i++)
+		try
 		{
-			Runnable task = () -> addVerticesAndEdges(graph, startLatch, endLatch);
-			executorService.execute(task);
+			for(int i = 0; i < 5; i++)
+			{
+				Runnable task = () -> addVerticesAndEdges(graph, startLatch, endLatch);
+				executorService.execute(task);
+			}
+
+			startLatch.countDown();
+			int v1 = graph.addVertex("Vertex 1 ");
+			int v2 = graph.addVertex("Vertex 2 ");
+			int v3 = graph.addVertex("Vertex 3 ");
+			int v4 = graph.addVertex("Vertex 4 ");
+			int v5 = graph.addVertex("Vertex 5 ");
+			endLatch.await();
+
+			Assert.assertEquals(30, graph.getVertices().size());
+
+			graph.addEdge(new Edge(v1, v2));
+			graph.addEdge(new Edge(v5, v4));
+			graph.addEdge(new Edge(v1, v3));
+			graph.addEdge(new Edge(v3, v5));
+			graph.addEdge(new Edge(v4, v2));
+
+			List<Edge> path = graph.getPath(v1, v4);
+			Assert.assertFalse(path.isEmpty());
 		}
-
-
-		startLatch.countDown();
-		int v1 = graph.addVertex("Vertex 1 ");
-		int v2 = graph.addVertex("Vertex 2 ");
-		int v3 = graph.addVertex("Vertex 3 ");
-		int v4 = graph.addVertex("Vertex 4 ");
-		int v5 = graph.addVertex("Vertex 5 ");
-		endLatch.await();
-
-		Assert.assertEquals(30, graph.getVertices().size());
-
-		graph.addEdge(new Edge(v1, v2));
-		graph.addEdge(new Edge(v5, v4));
-		graph.addEdge(new Edge(v1, v3));
-		graph.addEdge(new Edge(v3, v5));
-		graph.addEdge(new Edge(v4, v2));
-
-		List<Edge> path = graph.getPath(v1,v4);
-		Assert.assertFalse(path.isEmpty());
+		finally
+		{
+			executorService.shutdown();
+		}
 	}
 
 	private void addVerticesAndEdges(Graph<String,Edge> graph, CountDownLatch startLatch, CountDownLatch endLatch)
@@ -86,34 +92,40 @@ public class TestGraphMultiThreaded
 		ExecutorService executorService = Executors.newCachedThreadPool();
 
 
-		Callable<Integer[]> task = () -> addVerticesAndEdgesAndGetID(graph, startLatch);
-		Future<Integer[]> future1 = executorService.submit(task);
-		Future<Integer[]> future2 = executorService.submit(task);
-		Future<Integer[]> future3 = executorService.submit(task);
-		Future<Integer[]> future4 = executorService.submit(task);
-		Future<Integer[]> future5 = executorService.submit(task);
+		try
+		{
+			Callable<Integer[]> task = () -> addVerticesAndEdgesAndGetID(graph, startLatch);
+			Future<Integer[]> future1 = executorService.submit(task);
+			Future<Integer[]> future2 = executorService.submit(task);
+			Future<Integer[]> future3 = executorService.submit(task);
+			Future<Integer[]> future4 = executorService.submit(task);
+			Future<Integer[]> future5 = executorService.submit(task);
 
+			startLatch.countDown();
+			int vv12 = future1.get()[1];
+			int vv21 = future2.get()[0];
+			int vv22 = future2.get()[1];
+			int vv31 = future3.get()[0];
+			int vv32 = future3.get()[1];
+			int vv41 = future4.get()[0];
+			int vv42 = future4.get()[1];
+			int vv51 = future5.get()[0];
 
-		startLatch.countDown();
-		int vv12 = future1.get()[1];
-		int vv21 = future2.get()[0];
-		int vv22 = future2.get()[1];
-		int vv31 = future3.get()[0];
-		int vv32 = future3.get()[1];
-		int vv41 = future4.get()[0];
-		int vv42 = future4.get()[1];
-		int vv51 = future5.get()[0];
+			//Make graph connected
+			graph.addEdge(new Edge(vv12, vv21));
+			graph.addEdge(new Edge(vv22, vv31));
+			graph.addEdge(new Edge(vv32, vv41));
+			graph.addEdge(new Edge(vv42, vv51));
 
-		//Make graph connected
-		graph.addEdge(new Edge(vv12, vv21));
-		graph.addEdge(new Edge(vv22, vv31));
-		graph.addEdge(new Edge(vv32, vv41));
-		graph.addEdge(new Edge(vv42, vv51));
+			Assert.assertEquals(25, graph.getVertices().size());
 
-		Assert.assertEquals(25, graph.getVertices().size());
-
-		List<Edge> path = graph.getPath(5,20);
-		Assert.assertFalse(path.isEmpty());
+			List<Edge> path = graph.getPath(5, 20);
+			Assert.assertFalse(path.isEmpty());
+		}
+		finally
+		{
+			executorService.shutdown();
+		}
 	}
 
 	private Integer[] addVerticesAndEdgesAndGetID(Graph<String,Edge> graph, CountDownLatch startLatch)
